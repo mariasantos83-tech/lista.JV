b869540f8cfa4a89531d570db753de9d9b34b06d
MariaLoretto <maria_santos83@estudante.sesisenai.org.br>
2026-03-27 11:46:35 -0300

    add - exemplos

---
+++ ex06.js
@@ -0,0 +1,8 @@
+function LETRA() {
+    let letra = prompt("Digite uma letra").toLowerCase();
+     if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+        alert("Sua letra é uma vogal");
+    } else {
+        alert("Sua letra é uma consoante");
+    }
+}
